71ea5af4e0522aea8c596a622d8d4233e52bc6e6
Catalin <newuser@example.com>
2014-08-29 13:27:14 +0300

    first go at random stars

--- Moon.cpp
+++ Moon.cpp
@@ -1,28 +1,51 @@
 #include "Moon.h"
 #include <math.h>
 
+#include <iostream>
 
-void DrawCircle(double r,double cx,double cy)
+using namespace std;
+
+
+void DrawCircle(double r,double cx,double cy, double alpha)
 {
     double const PI = 3.14159;
     double w = 600.0, h = 400.0;
     glBegin(GL_POLYGON);
-        glColor4f( 255/255.0 , 250/255.0, 218/255.0, 0.6);
+        glColor4f( 255/255.0 , 242/255.0, 151/255.0, alpha);
         glVertex3f(cx,cy,0.0);
-
         for(double t = 0; t<PI*2;t+=0.2)
-        {
-            glColor4f( 255/255.0 , 250/255.0, 218/255.0, 0.1);
+        {   
+            glColor4f( 255/255.0 , 242/255.0, 151/255.0, alpha);
             glVertex3f(cx+r/w*cos(t),cy+r/h*sin(t),0.0);
         }
         glVertex3f(cx+r/w*cos(0.0),cy+r/h*sin(0.0),0.0);
     glEnd();
 }
 
+inline int in(int x, int min, int max) {
+    if(x > min && x < max) {
+        return true;
+    }
+    return false;
+}
+
 
 void Moon::drawMoon(double &xPos, double &yPos, double &radius) {
     glPushMatrix();
-        DrawCircle(radius, xPos, yPos);
+        int count = 0;
+        for(double i=10.0; i >= 6.0; i = i - 0.1) {
+            count ++;
+            if(in(count, 30, 40)) {
+                DrawCircle(radius * (i / 10.0), xPos, yPos, 0.25);
+            } else if(in(count, 20, 30)) {
+                DrawCircle(radius * (i / 10.0), xPos, yPos, 0.2);
+            } else if(in(count, 10, 20)) {
+                DrawCircle(radius * (i / 10.0), xPos, yPos, 0.15);
+            } else if(in(count, 0, 10)) {
+                DrawCircle(radius * (i / 10.0), xPos, yPos, 0.1);
+            }
+        }
+        DrawCircle(radius * 0.6, xPos, yPos, 0.6);
     glPopMatrix();
 
     // Animate 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "Earth.h"
 #include "Moon.h"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -9,6 +11,26 @@ double xPos = -0.8;
 double yPos = -0.3;
 double radius = 100;
 
+
+void drawRandomStars() {
+	srand(time(0));
+	double x, y;
+	glBegin(GL_POINTS);
+	for(double i = -1.0; i < 1.0; i+= 0.1) {
+		for(double j = -1.0; j < 1.0; j+= 0.1) {
+			x = i*10 + (rand() % (int)((i+0.1)*10 - i*10 + 10.0));
+			y = j*10 + (rand() % (int)((j+0.1)*10 - j*10 + 10.0));
+			if(x < (-10) || x > 10 || y < (-10) || y > 10) {
+				continue;
+			}
+			glColor3f( 0/255.0 , 0/255.0, 0/255.0);
+			glVertex3f(x, y, 0.0);
+			cout<<x/10.0<<" "<<y/10.0<<endl;
+		}
+	}
+	glEnd();
+}
+
 void initialize()
 {
 	glutInitWindowSize(600,400);
@@ -16,14 +38,14 @@ void initialize()
 	glClearColor(70 / 255.0, 66 / 255.0, 101 / 255.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-}
-void draw() {
 	glClear (GL_COLOR_BUFFER_BIT);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	
+	drawRandomStars();
+}
+void draw() {
 	Moon::drawMoon(xPos, yPos, radius);
-	cout<<xPos<<" "<<yPos<<endl;
 	Earth::draw();
 
 	glFlush();
